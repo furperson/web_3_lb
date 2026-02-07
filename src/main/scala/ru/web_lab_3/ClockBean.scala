8c405b3e6bdd0117c5caf58afbaaf11a3cf97046
Nikita Syschikov <>
2026-02-07 10:49:18 +0300

    done 3lab 2

--- src/main/scala/ru/web_lab_3/ClockBean.scala
+++ src/main/scala/ru/web_lab_3/ClockBean.scala
@@ -1,9 +1,16 @@
 package ru.web_lab_3
+
 import jakarta.inject.Named
 import jakarta.enterprise.context.RequestScoped
 import java.util.Date
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
 @Named("clockBean")
 @RequestScoped
 class ClockBean {
-  def getNow: Date = new Date()
+  def getNow: Date = {
+    val moscowTime = ZonedDateTime.now(ZoneId.of("Europe/Moscow"))
+    Date.from(moscowTime.toInstant())
+  }
 }
